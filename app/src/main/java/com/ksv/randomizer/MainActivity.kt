2708afd2327f5d9cc7b90ba866b75e3f0c273987
KotlyarovSergey <zews-sv@rambler.ru>
2023-11-28 18:19:49 +0300

    Left menu ClickListener now worked

--- app/src/main/java/com/ksv/randomizer/MainActivity.kt
+++ app/src/main/java/com/ksv/randomizer/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.view.GravityCompat
 import com.ksv.randomizer.databinding.ActivityMainBinding
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -15,53 +16,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
 
-        binding.navigationContainer
+        //binding.navigationContainer
 
         binding.toolbar.setNavigationOnClickListener {
-            // открфваем меню
             binding.navigationContainer.openDrawer(GravityCompat.START)
         }
 
         binding.leftMenu.setNavigationItemSelectedListener {
-            binding.tvTest.text = "Listenter"
-//            var intent: Intent? = null
-//            when (it.itemId){
-//                R.id.itmNumbers ->
-//                    intent = Intent(this, ActivityNumbers::class.java)
-//                R.id.itmLists ->
-//                    //intent = null
-//                    binding.tvTest.text = "itmLists"
-//                R.id.itmDice ->
-//                    intent = null
-//            }
-//            binding.navigationContainer.closeDrawer(GravityCompat.START)
-//            startActivity(intent)
-            val intent = Intent(this, ActivityNumbers::class.java)
-            startActivity(intent)
+            var intent: Intent? = null
+            when (it.itemId){
+                R.id.itmNumbers ->
+                    intent = Intent(this, ActivityNumbers::class.java)
+                R.id.itmLists ->
+                    intent = null
+                R.id.itmDice ->
+                    intent = null
+            }
+
+            binding.navigationContainer.closeDrawer(GravityCompat.START)
+            if(intent != null)  startActivity(intent)
+
             true
         }
 
 
     }
-
-    private fun openActivityNumbers(){
-        val intent = Intent(this, ActivityNumbers::class.java)
-        startActivity(intent)
-    }
-
-    fun onClickItmNumbers(view: View){
-        val intent = Intent(this, ActivityNumbers::class.java)
-        startActivity(intent)
-    }
-    fun onClickItmLists(view: View){
-//        val intent = Intent(this,  ::class.java)
-//        startActivity(intent)
-    }
-    fun onClickItmDice(view: View){
-//        val intent = Intent(this,  ::class.java)
-//        startActivity(intent)
-    }
-
 
 
 
