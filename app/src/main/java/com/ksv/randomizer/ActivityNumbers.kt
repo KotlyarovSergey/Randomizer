e3edd8cc895585b7ac163e090ff8a9316561426e
KotlyarovSergey <zews-sv@rambler.ru>
2023-12-04 15:03:44 +0300

    Number generation is worked

--- app/src/main/java/com/ksv/randomizer/ActivityNumbers.kt
+++ app/src/main/java/com/ksv/randomizer/ActivityNumbers.kt
@@ -2,6 +2,8 @@ package com.ksv.randomizer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.ksv.randomizer.databinding.ActivityNumbersBinding
 
 class ActivityNumbers : AppCompatActivity() {
@@ -10,5 +12,34 @@ class ActivityNumbers : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityNumbersBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        Log.i("ksvlog", "activity NUMBERS created")
+    }
+
+    fun onClickGenerate(view: View){
+//        Log.i("ksvlog", "onClick if worked")
+
+        var start = 0
+        var tmpString = binding.etRangeStart.text.toString()
+
+        if (!tmpString.isEmpty()){
+            start = tmpString.toInt()
+        }
+
+        var end = 0
+        tmpString = binding.etRangeEnd.text.toString()
+        if(!tmpString.isEmpty()){
+            end = tmpString.toInt()
+        }
+
+        if(start > end){
+            var tmpInt = end
+            end = start
+            start = tmpInt
+        }
+
+        val result = (start..end).random()
+        Log.i("ksvlog", "start: $start, end: $end, rand: $result")
+        binding.tvResult.text = result.toString()
     }
 }
