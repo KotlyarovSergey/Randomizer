240b28a483fc2b3dd05786a58e8a336a093eb5d7
KotlyarovSergey <zews-sv@rambler.ru>
2023-12-05 16:52:18 +0300

    Generate range of random numbers

--- app/src/main/java/com/ksv/randomizer/ActivityNumbers.kt
+++ app/src/main/java/com/ksv/randomizer/ActivityNumbers.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import com.ksv.randomizer.databinding.ActivityNumbersBinding
+import com.ksv.randomizer.numbers.NumbersSettingSet
 
 class ActivityNumbers : AppCompatActivity() {
     private lateinit var binding: ActivityNumbersBinding
@@ -19,12 +20,45 @@ class ActivityNumbers : AppCompatActivity() {
     fun onClickGenerate(view: View){
 //        Log.i("ksvlog", "onClick if worked")
 
+        checkedInput()
+
+        val settingSet = getSettings()
+
+        val randoms = getRandoms(settingSet)
+
+
+//        Log.i("ksvlog", "start: ${settingSet.start}, end: ${settingSet.end}, quantity: ${settingSet.quantity}")
+        Log.i("ksvlog", "range: ${settingSet.range}, quantity: ${settingSet.quantity}")
+        Log.i("ksvlog", "result: $randoms")
+
+        val result = randoms.joinToString(", ")
+        binding.tvResult.text = result
+    }
+
+
+
+    private fun getRandoms(settingSet: NumbersSettingSet): List<Int>{
+        val numbers = mutableListOf<Int>()
+        repeat(settingSet.quantity){
+            numbers.add(settingSet.range.random())
+        }
+        return numbers
+    }
+
+    private fun checkedInput(){
+
         if (binding.etRangeStart.text.isEmpty())
             binding.etRangeStart.setText("0")
 
         if(binding.etRangeEnd.text.isEmpty())
             binding.etRangeEnd.setText("0")
 
+        if(binding.etQuantityNumbers.text.isEmpty())
+            binding.etQuantityNumbers.setText("1")
+
+    }
+
+    private fun getSettings(): NumbersSettingSet{
         var start = binding.etRangeStart.text.toString().toInt()
         var end = binding.etRangeEnd.text.toString().toInt()
 
@@ -34,8 +68,13 @@ class ActivityNumbers : AppCompatActivity() {
             start = tmpInt
         }
 
-        val result = (start..end).random()
-        Log.i("ksvlog", "start: $start, end: $end, rand: $result")
-        binding.tvResult.text = result.toString()
+        var quantityNumbers = binding.etQuantityNumbers.text.toString().toInt()
+        if(quantityNumbers == 0) {
+            quantityNumbers = 1
+            binding.etQuantityNumbers.setText("1")
+        }
+
+        return NumbersSettingSet(start..end, quantityNumbers)
+
     }
 }
---
+++ app/src/main/java/com/ksv/randomizer/numbers/NumbersSettingSet.kt
@@ -0,0 +1,8 @@
+package com.ksv.randomizer.numbers
+
+import android.util.Range
+
+data class NumbersSettingSet(
+    val range: IntRange,
+    val quantity: Int
+)
