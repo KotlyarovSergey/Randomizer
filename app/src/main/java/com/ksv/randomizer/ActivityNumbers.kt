3d2e171619cdb1712609e06d00b9c3c2d205886a
KotlyarovSergey <zews-sv@rambler.ru>
2023-12-15 00:20:10 +0300

    Save/load ActivityNumbers state/data

--- app/src/main/java/com/ksv/randomizer/ActivityNumbers.kt
+++ app/src/main/java/com/ksv/randomizer/ActivityNumbers.kt
@@ -1,19 +1,24 @@
 package com.ksv.randomizer
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import com.ksv.randomizer.databinding.ActivityNumbersBinding
+import com.ksv.randomizer.numbers.ActNumberPreferences
 import com.ksv.randomizer.numbers.NumbersGenerator
 import com.ksv.randomizer.numbers.NumbersSettingSet
 
 class ActivityNumbers : AppCompatActivity() {
     private lateinit var binding: ActivityNumbersBinding
+    private lateinit var preferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNumbersBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        preferences = getSharedPreferences("settings", Context.MODE_PRIVATE)
 
         binding.switchLinear.setOnCheckedChangeListener(this::onCheckedChangeLinear)
         binding.switchShuffle.setOnCheckedChangeListener(this::onCheckedChangeShuffle)
@@ -21,6 +26,29 @@ class ActivityNumbers : AppCompatActivity() {
         Log.i("ksvlog", "activity NUMBERS created")
     }
 
+
+    override fun onResume() {
+        super.onResume()
+        binding.etRangeStart.setText(preferences.getString(ActNumberPreferences.RANGE_START, ""))
+        binding.etRangeEnd.setText(preferences.getString(ActNumberPreferences.RANGE_END, ""))
+        binding.etQuantityNumbers.setText(preferences.getString(ActNumberPreferences.NUMBERS_QUANTITY, ""))
+        binding.switchLinear.isChecked = preferences.getBoolean(ActNumberPreferences.LINEAR_GENERATION, true)
+        binding.switchShuffle.isChecked = preferences.getBoolean(ActNumberPreferences.SHUFFLE_RESULT, true)
+        binding.switchShuffle.isEnabled = preferences.getBoolean(ActNumberPreferences.SHUFFLE_ENABLED, true)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val prefSaver = preferences.edit()
+        prefSaver.putString(ActNumberPreferences.RANGE_START, binding.etRangeStart.text.toString()).apply()
+        prefSaver.putString(ActNumberPreferences.RANGE_END, binding.etRangeEnd.text.toString()).apply()
+        prefSaver.putString(ActNumberPreferences.NUMBERS_QUANTITY, binding.etQuantityNumbers.text.toString()).apply()
+        prefSaver.putBoolean(ActNumberPreferences.LINEAR_GENERATION, binding.switchLinear.isChecked).apply()
+        prefSaver.putBoolean(ActNumberPreferences.SHUFFLE_RESULT, binding.switchShuffle.isChecked).apply()
+        prefSaver.putBoolean(ActNumberPreferences.SHUFFLE_ENABLED, binding.switchShuffle.isEnabled).apply()
+
+
+    }
 
     fun onClickGenerate(view: View) {
 //        Log.i("ksvlog", "onClick if worked")
---
+++ app/src/main/java/com/ksv/randomizer/numbers/ActNumberPreferences.kt
@@ -0,0 +1,10 @@
+package com.ksv.randomizer.numbers
+
+object ActNumberPreferences {
+    const val RANGE_START = "range_start"
+    const val RANGE_END = "range_end"
+    const val NUMBERS_QUANTITY = "numbers_quantity"
+    const val LINEAR_GENERATION = "linear_generation"
+    const val SHUFFLE_ENABLED = "shuffle_enabled"
+    const val SHUFFLE_RESULT = "shuffle_result"
+}
